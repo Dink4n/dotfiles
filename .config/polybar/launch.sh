91d0aa558b335056c7b05501773c0249f1bbf0fa
JimboKearn <anujithbahuleyan@gmail.com>
2020-08-03 09:25:07 +0530

    made some changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar -r mybar >>/tmp/polybar1.log 2>&1 &
-# polybar bar2 >>/tmp/polybar2.log 2>&1 &
+polybar -r dummy >>/tmp/polybar2.log 2>&1 &
 
 echo "Bars launched..."
